ba2839e368af7fd708e98b212a02e3937ff6adc0
alegonzalezgar <belinus@Omega>
2023-03-03 10:08:00 -0500

    BasicConfig

---
+++ BasicConfig/Src/main.c
@@ -0,0 +1,24 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : alegonzalezgar
+ * @brief          : Configuracion basica de un proyecto
+ ******************************************************************************
+ *
+ ******************************************************************************
+ */
+#include <stdint.h>
+
+/**
+ * Funcion principal del programa.
+ * Esta funcion es el corazon del programa!!
+ **/
+int main(void){
+
+    /* Loop forever */
+	while(1){
+
+	}
+
+	return 0;
+}
